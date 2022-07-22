2aaeaddb71e3860a329dce2b4d65ee490f62ea32
vdukhovnik <vdukhovnik1@gmail.com>
2022-07-22 19:23:58 +0200

    Updates

---
+++ precache-manifest.c4325d3be8969039ac409ae25b7f7471.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c49971f07a5d2dabaee7a75d7f4175d",
+    "url": "/it-incubator-hw-from-Ignat/index.html"
+  },
+  {
+    "revision": "3b993fe4f3a6d1510fa5",
+    "url": "/it-incubator-hw-from-Ignat/static/css/main.c943ce37.chunk.css"
+  },
+  {
+    "revision": "2d734d1247645b7352cf",
+    "url": "/it-incubator-hw-from-Ignat/static/js/2.47edb5e4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/it-incubator-hw-from-Ignat/static/js/2.47edb5e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b993fe4f3a6d1510fa5",
+    "url": "/it-incubator-hw-from-Ignat/static/js/main.c70101d6.chunk.js"
+  },
+  {
+    "revision": "07d936231bce4b4a785d",
+    "url": "/it-incubator-hw-from-Ignat/static/js/runtime-main.8928210a.js"
+  }
+]);
