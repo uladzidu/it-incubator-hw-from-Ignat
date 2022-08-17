d0dd22d23c6dd971d78228d572d4337c9347f1ce
vdukhovnik <vdukhovnik1@gmail.com>
2022-08-17 16:50:26 +0200

    Updates

--- precache-manifest.d039b2f1f5b1d56ebb3c2dd3f00b3d09.js
+++ precache-manifest.d039b2f1f5b1d56ebb3c2dd3f00b3d09.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0033e325ff39a004b461484b46804b6f",
+    "revision": "afe6e12f7a2e5edd088671ca0badd500",
     "url": "/it-incubator-hw-from-Ignat/index.html"
   },
   {
-    "revision": "14f4d2f33e0afba95307",
-    "url": "/it-incubator-hw-from-Ignat/static/css/main.3ea35ada.chunk.css"
+    "revision": "baba3e98392e8aed5800",
+    "url": "/it-incubator-hw-from-Ignat/static/css/main.d906417e.chunk.css"
   },
   {
     "revision": "4c62e0c1101d848c50da",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/it-incubator-hw-from-Ignat/static/js/2.3db7304b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "14f4d2f33e0afba95307",
-    "url": "/it-incubator-hw-from-Ignat/static/js/main.9db0242e.chunk.js"
+    "revision": "baba3e98392e8aed5800",
+    "url": "/it-incubator-hw-from-Ignat/static/js/main.b27a9e9f.chunk.js"
   },
   {
     "revision": "7a3566cb20b98efe6717",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/it-incubator-hw-from-Ignat/precache-manifest.455a6591b9d9547446ba9a3e6762b300.js"
+  "/it-incubator-hw-from-Ignat/precache-manifest.d039b2f1f5b1d56ebb3c2dd3f00b3d09.js"
 );
 
 self.addEventListener('message', (event) => {
